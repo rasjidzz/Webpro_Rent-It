cfdb638cf43291cd88333e2110105ac6ab7d35b6
rasjidzz <mrisjads@gmail.com>
2023-12-20 16:11:03 +0700

    Upd : Admin side

--- resources/views/admin/reportPage/index.blade.php
+++ resources/views/admin/reportPage/index.blade.php
@@ -1,75 +1,79 @@
-<style>
-    * {
-        font-family: 'Open Sans', sans-serif;
-    }
-</style>
+@extends('layouts.admin')
 
-<div class="container-lg justify-content-center">
-    <h1 class="text-center mt-5 mb-5">Laporan Kerusakan</h1>
+@section('content')
+    <style>
+        * {
+            font-family: 'Open Sans', sans-serif;
+        }
+    </style>
 
-    <div class="card card-body" style="background-color: #F4F5F6;">
-        <table class="table table-responsive table-hover">
-            <thead style="background-color: #D32B31; color:azure">
-                <tr>
-                    <th scope="col" style="width: 200px;">Gedung</th>
-                    <th scope="col" style="width: 300px;">Peminjam</th>
-                    <th scope="col" style="width: 150px;">NIM</th>
-                    <th scope="col" style="width: 150px;">No Telepon</th>
-                    <th scope="col" style="width: 150px;">Dokumen</th>
-                    <th scope="col" style="width: 150px;">Tanggal Pinjam</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <img src="Assets/GedungPerkuliahan/GKU/GKU-1.jpg" alt="Gedung" width="90px" height="90px">
-                    </td>
-                    <td>
-                        <p>SURYA AULIA</p>
-                    </td>
-                    <td>
-                        <p>1302210084</p>
-                    </td>
-                    <td>
-                        081246296666
-                    </td>
-                    <td>
-                        <a href="Assets/PDF/PBO_MOD7_BELLA HUTAURUK_1301213327_SUI.pdf" download>docs1</a><br>
-                        <a href="Assets/PDF/TP_MOD7_1301210110_Maharani_Salsabila.pdf" download>docs2</a>
-                    </td>
-                    <td>
-                        Jumat, 12:30 - 15.30
-                    </td>
-                    <td class="align-middle">
-                        <button type="button" class="btn btn-success btn-lg" style="width: 150px;">Done</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <img src="Assets/GedungPerkuliahan/GKU/GKU-1.jpg" alt="Gedung" width="90px" height="90px" id="gambar_gedung">
-                    </td>
-                    <td>
-                        <p>AULIA SURYA</p>
-                    </td>
-                    <td>
-                        <p>4800122031</p>
-                    </td>
-                    <td>
-                        081246296666
-                    </td>
-                    <td>
-                        <a href="Assets/PDF/PBO_MOD7_BELLA HUTAURUK_1301213327_SUI.pdf" download>docs1</a><br>
-                        <a href="Assets/PDF/TP_MOD7_1301210110_Maharani_Salsabila.pdf" download>docs2</a>
-                    </td>
-                    <td>
-                        Selasa, 12:30 - 15.30
-                    </td>
-                    <td class="align-middle">
-                        <button type="button" class="btn btn-success btn-lg" style="width: 150px;">Done</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="container-lg justify-content-center">
+        <h1 class="text-center mt-5 mb-5">Laporan Kerusakan</h1>
+
+        <div class="card card-body" style="background-color: #F4F5F6;">
+            <table class="table table-responsive table-hover">
+                <thead style="background-color: #D32B31; color:azure">
+                    <tr>
+                        <th scope="col" style="width: 200px;">Gedung</th>
+                        <th scope="col" style="width: 300px;">Peminjam</th>
+                        <th scope="col" style="width: 150px;">NIM</th>
+                        <th scope="col" style="width: 150px;">No Telepon</th>
+                        <th scope="col" style="width: 150px;">Dokumen</th>
+                        <th scope="col" style="width: 150px;">Tanggal Pinjam</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src="Assets/GedungPerkuliahan/GKU/GKU-1.jpg" alt="Gedung" width="90px" height="90px">
+                        </td>
+                        <td>
+                            <p>SURYA AULIA</p>
+                        </td>
+                        <td>
+                            <p>1302210084</p>
+                        </td>
+                        <td>
+                            081246296666
+                        </td>
+                        <td>
+                            <a href="Assets/PDF/PBO_MOD7_BELLA HUTAURUK_1301213327_SUI.pdf" download>docs1</a><br>
+                            <a href="Assets/PDF/TP_MOD7_1301210110_Maharani_Salsabila.pdf" download>docs2</a>
+                        </td>
+                        <td>
+                            Jumat, 12:30 - 15.30
+                        </td>
+                        <td class="align-middle">
+                            <button type="button" class="btn btn-success btn-lg" style="width: 150px;">Done</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src="Assets/GedungPerkuliahan/GKU/GKU-1.jpg" alt="Gedung" width="90px" height="90px" id="gambar_gedung">
+                        </td>
+                        <td>
+                            <p>AULIA SURYA</p>
+                        </td>
+                        <td>
+                            <p>4800122031</p>
+                        </td>
+                        <td>
+                            081246296666
+                        </td>
+                        <td>
+                            <a href="Assets/PDF/PBO_MOD7_BELLA HUTAURUK_1301213327_SUI.pdf" download>docs1</a><br>
+                            <a href="Assets/PDF/TP_MOD7_1301210110_Maharani_Salsabila.pdf" download>docs2</a>
+                        </td>
+                        <td>
+                            Selasa, 12:30 - 15.30
+                        </td>
+                        <td class="align-middle">
+                            <button type="button" class="btn btn-success btn-lg" style="width: 150px;">Done</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
+@endsection
